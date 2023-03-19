62c929d9494f8e693beb1dbdba67acd461e828a9
Serge <smdyachenko@gmail.com>
2023-03-19 23:33:48 +0200

    2

--- 4-unsuccess-solution-of-3/code.js
+++ 4-unsuccess-solution-of-3/code.js
@@ -5,7 +5,6 @@
       console.log({ p });
       const src = fs.readFileSync(__dirname + '/index.js', 'utf-8')
       console.log({ src: src.substring(0, 11) });
-      throw new Error('TEST')
 
       (async function () {
          const asyncSrc = await fs.promises.readFile(__dirname + '/index.js', 'utf-8')
